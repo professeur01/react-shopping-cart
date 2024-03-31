5148542d00a8482858d367a88809cbb67190f5e9
Awal Massaoudou <awaloumassaoudou@gmail.com>
2024-03-31 22:23:27 +0100

    commit

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,33 @@
-   import { useGetAllProductsQuery } from "../features/productsApi";
-    const Home = () => {
-       const { data, error, isLoading }  = useGetAllProductsQuery();
-        return ( <h1>Home</h1> );
-    }
-     
-    export default Home;
+import { productsApi, useGetAllProductsQuery } from "../features/productsApi";
+console.log(productsApi)
+const Home = () => {
+  const { data, error, isLoading } = useGetAllProductsQuery();
+  return (
+    <div className="home-container">
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : error ? (
+        <p>An error occured</p>
+      ) : (
+        <>
+          <h1>Welcome to our store!</h1>
+          <div className="products">
+            {data?.map((product) => (
+              <div key={product.id} className="product" >
+                <h3>{product.name}</h3>
+                <img src={product.image} alt={product.name} />
+                <div className="details">
+                <span>{product.desc}</span>
+                  <span className="price">${product.price}</span>
+                </div>
+                <button>Add To Card</button>
+              </div>
+            ))}
+          </div>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default Home;
